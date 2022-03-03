989ce6e1876f696eef4496670db875c6b0196a17
Petr Kubeš <petrkubes1997@gmail.com>
2022-03-03 16:55:04 +0100

    game for perfomance testing

--- webRTCplayground/simplest/server/client/js/main.js
+++ webRTCplayground/simplest/server/client/js/main.js
@@ -1,34 +1,62 @@
 "use strict"
 
-// TODO: try to make it work according to this tutorial https://www.html5rocks.com/en/tutorials/webrtc/basics/
-
-const txtSend = document.querySelector("textarea#txtSend")
 const txtReceive = document.querySelector("textarea#txtReceive")
 
-const btnStart = document.querySelector("button#btnStart")
-const btnChannel = document.querySelector("button#btnChannel")
+const btnCreate = document.querySelector("button#btnCreate")
 const btnSend = document.querySelector("button#btnSend")
 const btnClose = document.querySelector("button#btnClose")
 
+const btnTop = document.querySelector("button#top")
+const btnDown = document.querySelector("button#down")
+
+const cnvsGame = document.querySelector("canvas#cnvsGame")
+
+///// GAME
+const ctx = cnvsGame.getContext("2d")
+ctx.fillStyle = "#a4ce81"
+ctx.fillRect(0, 0, 300, 300)
+
+let gameState = {
+    root: {
+        x: 0,
+        y: 0
+    },
+    node: {
+        x: 200,
+        y: 200
+    }
+}
+
+let isRoot = false
+
+/////
 const socket = io()
 
 const ROOT_OFFER = "root_offer"
 const NODE_OFFER = "node_offer"
 const ICE_OFFER = "root_ice_offer"
 
-let isRoot = false
 
 const connection = new RTCPeerConnection()
 
-let channel
-connection.ondatachannel = (channelEvent) => {
-    console.log(channelEvent)
-    channel = channelEvent.channel
+
+/////
+const listenToChannel = () => {
     channel.onmessage = (event) => {
-        txtReceive.value = event.data
-        console.log(event)
+        txtReceive.value = txtReceive.value.concat("\n" + event.data)
+        const parsed = JSON.parse(event.data)
+        console.log(parsed)
+        updateGameState(parsed)
     }
 }
+
+let channel
+connection.ondatachannel = (channelEvent) => {
+    console.log("ondatachannel", channelEvent)
+    channel = channelEvent.channel
+    listenToChannel()
+}
+
 connection.onicecandidate = (iceEvent) => {
     console.log("onIceCandidate:", iceEvent)
     if (iceEvent.candidate) {
@@ -40,28 +68,15 @@ connection.onicecandidate = (iceEvent) => {
     }
 }
 
-// localConnection.onicecandidate = e => !e.candidate
-//     || remoteConnection.addIceCandidate(e.candidate)
-//         .catch(handleAddCandidateError);
-
-
-const sendData = () => {
-    channel.send("testing message")
+const sendData = (data) => {
+    const stringified = JSON.stringify(data)
+    channel.send(stringified)
 }
 
 const initializeConnection = () => {
     isRoot = true
-
-    const channelChangedCallback = (e) => {
-        console.log("channel changed:", e)
-    }
-
     channel = connection.createDataChannel("sendDataChannel", null)
-    channel.onopen = channelChangedCallback
-    channel.onclose = channelChangedCallback
-    channel.onmessage = (event) => {
-        txtReceive.value = event.data
-    }
+    listenToChannel()
 
     connection.createOffer()
         .then(offer => connection.setLocalDescription(offer))
@@ -70,7 +85,6 @@ const initializeConnection = () => {
 }
 
 
-btnStart.disabled = true
 btnClose.disabled = false
 
 socket.on("message", (message) => {
@@ -93,21 +107,83 @@ socket.on(NODE_OFFER, (message) => {
 
 socket.on(ICE_OFFER, (message) => {
     const candidate = new RTCIceCandidate({
-        sdpMLineIndex: message.label,
-        candidate: message.candidate
+        sdpMLineIndex: message.label, candidate: message.candidate
     })
     connection.addIceCandidate(candidate)
 })
 
 const sendMessage = (channel, message) => {
     console.log("Client sending message: ", message, channel)
-    socket.emit('message', {channel, message})
+    socket.emit("message", {channel, message})
 }
 
 const handleDescriptionError = (e) => console.error(e)
 
-
-btnChannel.onclick = initializeConnection
+btnCreate.onclick = initializeConnection
 btnSend.onclick = sendData
-// btnClose.onclick = closeDataChannels
+
+btnTop.onclick = () => {
+    if (isRoot) {
+        updateGameState(
+            {
+                ...gameState,
+                root: {
+                    ...gameState.root,
+                    y: gameState.root.y - 10
+                }
+            })
+    } else {
+        sendData(
+            {
+                ...gameState,
+                node: {
+                    ...gameState.node,
+                    y: gameState.node.y - 10
+                }
+            }
+        )
+    }
+}
+
+btnDown.onclick = () => {
+    if (isRoot) {
+        updateGameState(
+            {
+                ...gameState,
+                root: {
+                    ...gameState.root,
+                    y: gameState.root.y + 10
+                }
+            })
+    } else {
+        // TODO, send only the delta
+        sendData(
+            {
+                ...gameState,
+                node: {
+                    ...gameState.node,
+                    y: gameState.node.y + 10
+                }
+            }
+        )
+    }
+}
+
+
+const updateGameState = (newGameState) => {
+    gameState = newGameState
+    if (isRoot) {
+        sendData(newGameState)
+    }
+
+    // Resets
+    ctx.fillStyle = "#a4ce81"
+    ctx.fillRect(0, 0, 300, 300)
+
+    ctx.fillStyle = "#ff4444"
+    ctx.fillRect(newGameState.root.x, newGameState.root.y, 50, 50)
+
+    ctx.fillStyle = "#467eff"
+    ctx.fillRect(newGameState.node.x, newGameState.node.y, 50, 50)
+}
 
