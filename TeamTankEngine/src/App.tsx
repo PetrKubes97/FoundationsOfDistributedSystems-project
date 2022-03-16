a23f8d9858849b2b2d2cdf6bb7935d3bb8704f59
Uppah <philip.thorsen@live.dk>
2022-03-16 17:17:11 +0100

    wall + wall collision

--- TeamTankEngine/src/App.tsx
+++ TeamTankEngine/src/App.tsx
@@ -1,36 +1,14 @@
 import './App.css'
 
-import TankGame from './TankGame'
+import PixiComp from './PixiShit'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>The Great Game of Tanks</p>
-
-        <div style={{width: '100%'}}>
-            <div style={{width: '50%', height: '100px', float: 'left'}}> 
-                <div>You:</div>
-                <img src="./src/tank.png" className="App-logo" width={50} height={50}/>
-            </div>
-            <div style={{marginLeft: '50%', height: '100px'}}> 
-                <div>Opponent:</div>
-                <img src="./src/OpponentTank.png" className="App-logo" width={50} height={50}/>
-            </div>
-        </div>
-  
-        <TankGame/>
-
-        <a
-            className="App-link"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source Code
-        </a>
-
+        <div>The Great Game of Tanks</div>
+        <PixiComp/>
       </header>
     </div>
   )
