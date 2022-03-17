8e1ba70d58e017949ed8ee17336507fe0273116f
Mathilde T <55983203+s184376@users.noreply.github.com>
2022-03-17 16:35:27 +0100

    channel creation button

--- TeamTankEngine/src/App.tsx
+++ TeamTankEngine/src/App.tsx
@@ -1,26 +1,43 @@
 import './App.css'
 
+import React, { useState } from "react";
+
 import TankGame from './TankGame'
 
+
 function App() {
+
+  const [channelCreated, setChannelCreated] = useState(false);
+
+  const onCreateChannelClick = () => {
+    setChannelCreated(true);
+    console.log('Here we insert the channel creation stuff')
+  }
 
   return (
     <div className="App">
       <header className="App-header">
       <p>The Great Game of Tanks</p>
 
-      <div style={{width: '100%'}}>
-          <div style={{width: '50%', height: '100px', float: 'left'}}> 
-              <div>You:</div>
-              <img src="./src/images/tank.png" className="App-logo" width={50} height={50}/>
+      
+      <button onClick={onCreateChannelClick}>Create channel</button>
+      {
+        channelCreated ? 
+        <>
+          <div style={{width: '100%'}}>
+            <div style={{width: '50%', height: '100px', float: 'left'}}> 
+                <div>You:</div>
+                <img src="./src/images/tank.png" className="App-logo" width={50} height={50}/>
+            </div>
+            <div style={{marginLeft: '50%', height: '100px'}}> 
+                <div>Opponent:</div>
+                <img src="./src/images/OpponentTank.png" className="App-logo" width={50} height={50}/>
+            </div>
           </div>
-          <div style={{marginLeft: '50%', height: '100px'}}> 
-              <div>Opponent:</div>
-              <img src="./src/images/OpponentTank.png" className="App-logo" width={50} height={50}/>
-          </div>
-      </div>
-
-      <TankGame/>
+          <TankGame/> 
+        </>
+        : null
+      }
 
       <a
           className="App-link"
@@ -31,7 +48,7 @@ function App() {
           Source Code
       </a>
       </header>
-    </div>
+    </div> 
   )
 }
 
