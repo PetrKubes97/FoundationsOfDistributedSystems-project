7f29173e0cd65964206a04ec40055eaa10fe9fe3
Matej Majtan <majtan.matej@gmail.com>
2022-03-17 19:07:29 +0100

    Clean up and new layout

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("message", ({ channel, message }) => {
+    console.log("broadcasting: ", message);
+    socket.broadcast.emit(channel, message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
