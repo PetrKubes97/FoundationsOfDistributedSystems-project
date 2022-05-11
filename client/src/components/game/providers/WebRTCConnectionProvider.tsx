a8c9833acfdb04c72b8873282d06e5a5c79a3663
Petr Kubes <petrkubes1997@gmail.com>
2022-05-11 23:39:49 +0200

    try more stun servers

--- client/src/components/game/providers/WebRTCConnectionProvider.tsx
+++ client/src/components/game/providers/WebRTCConnectionProvider.tsx
@@ -8,20 +8,14 @@ const ICE_OFFER = 'ice_offer'
 const configuration = {
   iceServers: [
     {
-      urls: 'stun:openrelay.metered.ca:80',
+      urls: ['stun:openrelay.metered.ca:80', 'stun:stun.l.google.com:19302'],
     },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject',
-    },
-    {
-      urls: 'turn:openrelay.metered.ca:443',
-      username: 'openrelayproject',
-      credential: 'openrelayproject',
-    },
-    {
-      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      urls: [
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp',
+      ],
       username: 'openrelayproject',
       credential: 'openrelayproject',
     },
