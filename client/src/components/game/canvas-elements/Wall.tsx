e99cdc30e87054e14596cb609f14f183f839c70b
Matej Majtan <majtan.matej@gmail.com>
2022-05-11 23:26:03 +0200

    fixed routing for wall resource

--- client/src/components/game/canvas-elements/Wall.tsx
+++ client/src/components/game/canvas-elements/Wall.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Container, Sprite } from '@inlet/react-pixi'
 import { Wall as WallType } from '../game-logic/Game'
+import WallImage from './src/images/wall.jpg'
 
 interface Props {
   coordinates: WallType[]
 }
 
 export const Wall: React.FC<Props> = ({ coordinates }) => {
-  const image = './src/images/wall.jpg'
-
   return (
     <Container>
       {coordinates &&
@@ -17,7 +16,7 @@ export const Wall: React.FC<Props> = ({ coordinates }) => {
             x={c.x}
             y={c.y}
             anchor={0}
-            image={image}
+            image={WallImage}
             width={c.size}
             height={c.size}
           />
